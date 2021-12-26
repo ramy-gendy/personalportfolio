878ddd15288e35d614fa233153dc44b6d0ef83df
ramy-gendy <61162010+ramy-gendy@users.noreply.github.com>
2021-12-26 09:00:49 +0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,64 @@
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import React from "react";
+import About from "../components/About";
+import Blog from "../components/Blog";
+import Contact from "../components/Contact";
+import Header from "../components/Header";
+import Intro from "../components/Intro";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import Testimonials from "../components/Testimonials";
+import getPosts from "../utils/getPosts";
+import { PostType } from "../types/PostType";
+
+interface Props {
+  Posts: [PostType];
+}
+
+const Home: React.FC<Props> = ({ Posts }) => {
+  return (
+    <div>
+      <Head>
+        <meta
+          name="keywords"
+          content="Avneesh, Agarwal, Avneesh Agarwal, web dev, blogger, content creator"
+        />
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="https://www.avneesh.tech/rss.xml"
+        />
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+
+      <div className="h-[10vh]">
+        <Header />
+      </div>
+
+      <main className="relative mb-10 ml-5">
+        <Intro />
+        <About />
+        <Testimonials />
+        <Projects />
+        <Skills />
+        <Blog Posts={Posts} />
+        <Contact />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const Posts = await getPosts("avneesh0612");
+
+  return {
+    props: {
+      Posts: Posts.publication.posts,
+    },
+    revalidate: 600,
+  };
+};
